667e37db3176c7588e4acd64d7a8d4c3a3fb2ac6
ncreel <nick.creel@protonmail.com>
2021-03-25 15:10:33 -0400

    implement web page server

--- src/server.js
+++ src/server.js
@@ -1,13 +1,27 @@
 const http = require('http');
-const products = require('./products.json')
+const products = require('./products.json');
+const fs = require('fs').promises;
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((request, response) => {
-  response.statusCode = 200;
-  response.setHeader('Content-Type', 'application/json');
-  response.end(JSON.stringify(products));
+
+  fs.readFile(__dirname + '/index.html')
+    .then(contents => {
+      response.statusCode = 200;
+      response.setHeader('Content-Type', 'text/html');
+      response.end(contents);
+    })
+    .catch(err => {
+      res.writeHead(500);
+      res.end(err);
+      return;
+    });
 });
+
+server.on('clientError', (err, socket)=> {
+  socket.end('HTTP/1.1 400 Bad Request\r\n\r\n')
+})
 
 server.listen(port, hostname, () =>{
   console.log(`Server running at http://${hostname}:${port}/`)
