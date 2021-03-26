7f9178c418eaf6136bd5bb5095abd282a9576565
ncreel <nick.creel@protonmail.com>
2021-03-26 13:49:19 -0400

    implement API for products, simple product search (will not run well for large arrays!! need to fix with a different search algorithm), search form now queries API

--- src/server.js
+++ src/server.js
@@ -3,16 +3,22 @@ const app = express();
 const products = require('./products.json');
 const fs = require('fs').promises;
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 3001;
 
 app.use(express.static('public'))
 
 app.get('/product', (req, res)=>{
-  res.send('hello world')
+  let productID = req.query.productID;
+  for(let i=0; i < products.records.length; i++){
+    if(products.records[i].id == productID){
+      return res.send(products.records[i])
+    }
+  }
+  return res.status(404).send("Sorry, that product doesn't exist!")
 })
 
 app.listen(port, ()=>{
-  console.log(`app listening at http://localhost:${3000}`)
+  console.log(`app listening at http://localhost:${port}`)
 })
 
 app.use(function (req, res, next) {
