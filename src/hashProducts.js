f15cad0452fa1498946191229d3b41050567203d
ncreel <nick.creel@protonmail.com>
2021-03-26 14:02:26 -0400

    create hashProducts.js function component, remove index.js (for now)

---
+++ src/hashProducts.js
@@ -0,0 +1,18 @@
+const products = require("./products.json");
+
+export function hashProducts(){
+  /* This funciton creates a hash table where the unique id of the products
+     is the key and the position of the product object in the records
+     array is the value. This is intended to speed up the search for each
+     client by avoiding a linear search; In this case, the records array
+     only needs to be traversed in linear order once to create the hash table,
+     rather than being traversed each time a search is made. Of course, this
+     is a linear search, so for a massive database of products, this initial
+     traversal is going to be very slow. 
+  */
+    let productHashTable = {};
+    for(let i=0; i<products.records.length; i++){
+      productHashTable[products.records[i].id] = i;
+    }
+    return productHashTable;
+}
