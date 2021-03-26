94c71370c7f41c897fbeac7cf34d60e705080ee1
ncreel <nick.creel@protonmail.com>
2021-03-26 15:39:09 -0400

    write event handler for search button

--- src/searchHandler.js
+++ src/searchHandler.js
@@ -1,18 +1,16 @@
-
-
 async function _fetchProduct(id){
   let _URL=`/product/${id}`;
   const response = await fetch(_URL, {method: 'GET', mode: 'same-origin'});
   if(response.status == 200){
-
+    // use object to render something on the page
   } else {
-    
+    // throw an error
   }
 }
 
 export function searchHandler (event){
   let searchBox = document.getElementById('productID');
   if(searchBox.value!=""){
-
+    _fetchProduct(searchBox.value)
   }
 }
