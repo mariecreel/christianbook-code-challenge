668c746438e1f6ee1c1632b45865119bc9a08ef8
ncreel <nick.creel@protonmail.com>
2021-03-26 15:40:48 -0400

    add comments to _fetchProduct function

--- src/searchHandler.js
+++ src/searchHandler.js
@@ -1,6 +1,7 @@
 async function _fetchProduct(id){
   let _URL=`/product/${id}`;
   const response = await fetch(_URL, {method: 'GET', mode: 'same-origin'});
+    // we're making a request to our own server, hence same-origin (not CORS)
   if(response.status == 200){
     // use object to render something on the page
   } else {
